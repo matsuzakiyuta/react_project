2f8cd3dea76f3a428afe967b65aa829054814275
matsuzakiyuta <ym0924dmg@gmail.com>
2021-01-23 16:26:49 +0900

    first commit
    Reactプロジェクト作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Animation_management</h1>
       </header>
+      <body className="App-body">
+        <Button className="btn btn-demo">アニメ一覧</Button>
+        <Button className="btn btn-warning">アニメ追加</Button>
+        <Button className="btn btn-danger">アニメ削除</Button>
+      </body>
     </div>
   );
 }
